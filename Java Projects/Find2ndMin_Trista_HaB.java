7f6f4303212ea43b80b461718d60d71ee7f73c75
benjaminha14 <benjaminha14@gmail.com>
2016-01-12 10:25:30 -0800

    Add semi working find 2nd min

--- Java Projects/Find2ndMin_Trista_HaB.java
+++ Java Projects/Find2ndMin_Trista_HaB.java
@@ -1,51 +1,57 @@
+//Kaito Trias and Benjamin Ha
+//AList-A3: Find2ndMin-PP
+
 import java.util.*;
 public class Find2ndMin_Trista_HaB
 {
-   public static void main ( String[] args)
+   public static void main (String[]args)
    {
-   ArrayList<Integer> stuff = new ArrayList<Integer>();
-   stuff.add(-2);
-   stuff.add(12);
-   stuff.add(22);
-   stuff.add(17);
-   stuff.add(19);
-   stuff.add(-10);
-   stuff.add(-2);
-   System.out.println("Test 1: 2nd smallest is: " + find2ndMin(stuff) );
+      ArrayList<Integer> stuff = new ArrayList<Integer>();
+      stuff.add(-2);
+      stuff.add(12);
+      stuff.add(22);
+      stuff.add(17);
+      stuff.add(19);
+      stuff.add(-10);
+      stuff.add(-2);
+      System.out.println("Test 1: 2nd smallest is: " + find2ndMin(stuff) );
 
-   stuff = new ArrayList<Integer>();
-   stuff.add(-2);
-   stuff.add(12);
-   stuff.add(22);
-   stuff.add(17);
-   stuff.add(19);
-   stuff.add(-1);
-   stuff.add(-2);
-   System.out.println("Test 2: 2nd smallest is: " + find2ndMin(stuff) );
+      stuff = new ArrayList<Integer>();
+      stuff.add(-2);
+      stuff.add(12);
+      stuff.add(22);
+      stuff.add(17);
+      stuff.add(19);
+      stuff.add(-1);
+      stuff.add(-2);
+      System.out.println("Test 2: 2nd smallest is: " + find2ndMin(stuff) );
 
-   stuff = new ArrayList<Integer>();
-   stuff.add(-2);
-   stuff.add(12);
-   stuff.add(22);
-   stuff.add(17);
-   stuff.add(19);
-   stuff.add(-1);
-   stuff.add(999999);
-   System.out.println("Test 3: 2nd smallest is: " + find2ndMin(stuff) );
-   }
-   public static int find2ndMin( ArrayList<Integer> ints )
+      stuff = new ArrayList<Integer>();
+      stuff.add(-2);
+      stuff.add(12);
+      stuff.add(22);
+      stuff.add(17);
+      stuff.add(19);
+      stuff.add(-1);
+      stuff.add(999999);
+      System.out.println("Test 3: 2nd smallest is: " + find2ndMin(stuff) );
+  }
+   public static int find2ndMin(ArrayList<Integer> ints)
    {
-      int min = ints.get(0);
-      int secondMin = ints.get(0);
-      for(int i:ints){
-         if(i<min && min < secondMin)
+      int min = 0;
+      int secondmin = 0;
+      for(int i = 0; i<ints.size(); i++)
+      {
+         if(ints.get(i)<min)
          {
-            secondMin = min;
-            min = i;
+            secondmin = min;
+            min = ints.get(i);
+         }
+         else if(ints.get(i)<secondmin)
+         {
+            secondmin = ints.get(i);
          }
       }
-      return secondMin;
+      return secondmin;
    }
-
-
 }
